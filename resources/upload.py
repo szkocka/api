1ae9ccc98df15172e95a27f2a3be9699f7e0a3d0
mkavf <mykola.kavf@gmail.com>
2015-09-03 19:04:17 +0300

    finished upload api

--- resources/upload.py
+++ resources/upload.py
@@ -1,8 +1,11 @@
 from boto.s3.key import Key, boto
+from bson import ObjectId
 from flask import request
 from flask.ext.restful import Resource
 from security.authenticate import authenticate
 
+BUCKET_NAME = 'srg-images'
+IMAGE_TYPE = 'image/jpeg'
 
 class Upload(Resource):
     method_decorators = [authenticate]
@@ -11,9 +14,13 @@ class Upload(Resource):
         self.db = kwargs['db']
 
     def post(self, current_user):
-        c = boto.connect_s3()
-        b = c.get_bucket('srg-images')
-        k = b.new_key('foobar3')
-        k.set_contents_from_file(request.files['file'])
+        uploaded_file = request.files['file']
 
-        return current_user.json(), 200
+        s3connection = boto.connect_s3()
+        bucket = s3connection.get_bucket(BUCKET_NAME)
+        key = bucket.new_key('{0}.jpg'.format(str(ObjectId())))
+        key.content_type = IMAGE_TYPE
+        key.set_contents_from_file(uploaded_file)
+        key.make_public()
+
+        return {"url": key.generate_url(expires_in=0, query_auth=False, force_http=True)}, 201
