8ef55715c2e63513f972b1b6422fa3e73883938e
Mykola Kavf <mkavf>
2016-04-05 14:23:47 +0300

    reversed response format for compatibility with UI

--- apis/me.py
+++ apis/me.py
@@ -10,9 +10,13 @@ class Me(Resource):
     def get(self, current_user):
         return ok(
             {
-                '_id': current_user.key.id(),
-                'name': current_user.name,
-                'email': current_user.email,
-                'role': 'admin' if current_user.is_admin else 'user'
+                'user':
+                    {
+                        '_id': current_user.key.id(),
+                        'name': current_user.name,
+                        'email': current_user.email,
+                        'role': 'admin' if current_user.is_admin else 'user',
+                        'provider': 'local'
+                    }
             }
         )
